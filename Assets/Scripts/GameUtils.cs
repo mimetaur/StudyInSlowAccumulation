766b57cf996f768984ac47e66942624f40b3c1b8
Nathan Koch <nathanrkoch@gmail.com>
2018-03-22 19:54:39 -0400

    coming together

---
+++ Assets/Scripts/GameUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameUtils
+{
+
+    public static Vector3 RandomPointWithinCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
+
+}
+
---
+++ Assets/Scripts/SpawnBalls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour
+{
+
+    public GameObject ball;
+    public GameObject floor;
+    public Material[] ballMaterials;
+    public float spawnRate = 5.0f;
+    public float spawnHeight = 50.0f;
+
+    public Vector3 floorCenter = new Vector3(0, 0, 0);
+    public float floorRadius = 50.0f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBall", spawnRate, spawnRate);
+    }
+
+    void SpawnBall()
+    {
+
+        Material newMat = ballMaterials[Random.Range(0, ballMaterials.Length)];
+
+        Vector3 pos = GetRandomSpawnPosition();
+        GameObject newBall = Instantiate(ball, pos, Quaternion.identity);
+        newBall.GetComponent<Renderer>().material = newMat;
+    }
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        Vector3 center = new Vector3(floorCenter.x, floorCenter.y + spawnHeight, floorCenter.z);
+        Vector3 pos = GameUtils.RandomPointWithinCircle(center, floorRadius);
+        return pos;
+    }
+}
