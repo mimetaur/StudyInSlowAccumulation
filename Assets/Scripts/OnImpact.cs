311956915f773f74efa8de2352d912fbe9cb558e
Nathan Koch <nathanrkoch@gmail.com>
2018-03-22 21:02:15 -0400

    close to done

---
+++ Assets/Scripts/OnImpact.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnImpact : MonoBehaviour
+{
+
+    private PlaySounds playSounds;
+    private Renderer rend;
+
+    private void Start()
+    {
+        var gameManager = GameObject.Find("GameManager");
+        rend = this.gameObject.GetComponent<Renderer>();
+        playSounds = gameManager.GetComponent<PlaySounds>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (!rend.isVisible)
+        {
+            return;
+        }
+
+        if (col.gameObject.tag == "Floor")
+        {
+            playSounds.PlayFloorSound();
+        }
+        else if (col.gameObject.tag == "Ball")
+        {
+            playSounds.PlayBallSound();
+        }
+    }
+}
---
+++ Assets/Scripts/PlaySounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour
+{
+
+    public AudioSource source;
+    public AudioClip[] floorClips;
+    public AudioClip[] ballClips;
+    public float ballVolume = 0.5f;
+    public float floorVolume = 1.0f;
+
+    public void PlayFloorSound()
+    {
+        AudioClip clip = floorClips[Random.Range(0, floorClips.Length)];
+        source.PlayOneShot(clip, floorVolume);
+    }
+
+    public void PlayBallSound()
+    {
+        print("col");
+        AudioClip clip = ballClips[Random.Range(0, ballClips.Length)];
+        source.PlayOneShot(clip, ballVolume);
+    }
+}
